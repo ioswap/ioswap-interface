434454ca589e95b754fd069696c729d8ad1ccdd7
iosdev <ioswap.service@gmail.com>
2021-08-09 14:36:38 +0800

    url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -17,28 +17,19 @@ export function isAddress(value: any): string | false {
   }
 }
 
-const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
-  1: '',
-  3: 'ropsten.',
-  4: 'rinkeby.',
-  5: 'goerli.',
-  42: 'kovan.',
-  66: 'okt.'
-}
-
 export function getEtherscanLink(
   chainId: ChainId,
   data: string,
   type: 'transaction' | 'token' | 'address' | 'block'
 ): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
+  const prefix = `https://www.oklink.com/okexchain`
 
   switch (type) {
     case 'transaction': {
       return `${prefix}/tx/${data}`
     }
     case 'token': {
-      return `${prefix}/token/${data}`
+      return `${prefix}/tokenAddr/${data}`
     }
     case 'block': {
       return `${prefix}/block/${data}`
